fd2a92f89fb502e8ffcada2387a210e83364a15b
triixoo <karaulsyngys89@gmail.com>
2024-09-13 19:49:43 +0500

    IDK

---
+++ src/Easytasks/Remove_Element.java
@@ -0,0 +1,17 @@
+package Easytasks;
+
+public class Remove_Element {
+    public class Solution{
+        public int removeElement(int[] nums, int val){
+            int k = 0;
+
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] != val ){
+                    nums[k] = nums[i];
+                    k++;
+                }
+            }
+            return k;
+        }
+    }
+}
