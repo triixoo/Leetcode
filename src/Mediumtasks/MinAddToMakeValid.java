55d28a63ef53271b661eb5f7f3442cdc1351b629
triixoo <karaulsyngys89@gmail.com>
2024-10-14 23:32:48 +0500

    Medium new tasks

--- src/Mediumtasks/MinAddToMakeValid.java
+++ src/Mediumtasks/MinAddToMakeValid.java
@@ -1,3 +1,26 @@
 package Mediumtasks;
+public class MinAddToMakeValid {
+    class Solution {
+        public int minAddToMakeValid(String s) {
+            int openBrackets = 0; // Tracks unmatched '('
+            int unmatchedClose = 0; // Tracks unmatched ')'
 
+            for (char c : s.toCharArray()) {
+                if (c == '(') {
+                    openBrackets++;
+                } else if (c == ')') {
+                    if (openBrackets > 0) {
+                        openBrackets--; // We have a matching '('
+                    } else {
+                        unmatchedClose++; // We have an unmatched ')'
+                    }
+                }
+            }
+
+            // The result is the sum of unmatched '(' and unmatched ')'
+            return openBrackets + unmatchedClose;
+        }
+    }
+
+}
 
